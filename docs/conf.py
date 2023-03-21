6aaf3cd99c67a497a6747ad4f41b0b4fda7e3591
ekauffma <elmaka8700@gmail.com>
2023-03-21 09:54:02 +0000

    added nbsphinx to requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -17,14 +17,7 @@ version = "0.1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "nbsphinx",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.napoleon",
-    "sphinx_copybutton",
-    "sphinxcontrib.programoutput",
-]
+extensions = ["nbsphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
